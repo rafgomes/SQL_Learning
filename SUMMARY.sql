04a277ed2a4207b7eb50531ca0d486219e710b58
rafgomes <rafgomes@gmail.com>
2023-02-07 22:02:12 -0300

    Tipos De Dados class

--- SUMMARY.sql
+++ SUMMARY.sql
@@ -87,11 +87,40 @@ SELECT coluna FROM TabelaA, TabelaB WHERE CONDICAO
 
 /*-------------------------STRING OPERATIONS-------------------------*/
 --Filtros com operações de string (maiusculo, minusculo, subistituir caracteres...)
---https://learn.microsoft.com/en-us/sql/t-sql/functions/string-functions-transact-sql?view=sql-server-ver16
+-- https://learn.microsoft.com/en-us/sql/t-sql/functions/string-functions-transact-sql?view=sql-server-ver16
 
 /*-------------------------FUNÇÕES MATEMATICAS-------------------------*/
 --Filtros com operações matematicas
-https://learn.microsoft.com/pt-br/sql/t-sql/functions/mathematical-functions-transact-sql?view=sql-server-ver16
+-- https://learn.microsoft.com/pt-br/sql/t-sql/functions/mathematical-functions-transact-sql?view=sql-server-ver16
+
+/*-------------------------TIPOS DE DADOS-------------------------*/
+--Boleanos
+--Tipo BIT-- Por padrão é inicado com NULL, e pode receber tanto 1 ou 0 
+
+--Caracteres
+--Tipo CHAR-- Permite inserir uma quantidade FIXA de caracteres e sempre ocupa todo o espaço reservado 10/50(por exemplo, se definiu como 50 e utilizou 10, ele vai ocupar os 50 espaços da memoria) 
+--Tipo VARCHAR / NVARCHAR -- Permite inserir uma quantidade VARIAVEL, porem dentro do limite definido 10/50 (Se utilizar apenas 10 espaços, irá ocupar somente 10, mas no maximo 50)
+
+--Numeros: 
+--VALORES EXATOS--
+--TINYINT-- Apenas valores inteiros no intervalo de 0 a 255
+--SMALLINT-- Apenas Valores inteiros no intevlao -32.768 a 32.767
+--INT-- Apenas valores inteiros no intevalo -2.147.483.648 a 2.147.483.647
+--BIGINT-- Apenas valores inteiros no intevalo -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807
+
+--VALORES APROXIMADOS--
+--NUMERIC / DECIMAL-- Valores exatos porem permite ter parte fracionado, e pode ser especificado a precisão -Ex: NUMERIC(5,2) retorna 113,44 (Total 5 digitos e 2 após a virgula
+--REAL / FLOAT-- Tem precisão de até 15 digitos após a virgula
+
+--Temporais:
+--DATE-- Armazena data, no formato de aaaa/mm/dd
+--DATETIME-- Armazena data e horas, no formato de aaaa/mm/dd:hh:mm:ss
+--DATETIME2-- Armazena data e horas com adição de milissegundos, no formato de aaaa/mm/dd:hh:mm:sssssssss
+--SMALLDATETIME-- Armazena data e horas respeitnado os limites '1990-01-01:00:00:00' até '2079-06-06:23:59:59'
+--TIME-- Armazena apenas horas, minutos, segundos e milissegundos dentro do limite '00:00:00.0000000' até '23:59:59.9999999'
+--DATETIMEOFFSET-- Armazena informações de data e horas incluido fuso horario
+
+
 
 
 
